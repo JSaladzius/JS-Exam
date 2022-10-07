b6aa60cdf9c02d2db70023e611c44ece818ff867
bulacio <postiklu@gmail.com>
2022-10-07 22:51:17 +0300

    Task 8 finished function Calculator created

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,27 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator(a, b) {
+  this.a = a;
+  this.b = b;
+  console.log("Results are:");
+  this.sum = function () {
+    console.log(`Sum:` + ` ` + `${this.a + this.b}`);
+  };
+  this.subtraction = function () {
+    console.log(`Difference:` + ` ` + `${this.a - this.b}`);
+  };
+  this.multiplication = function () {
+    console.log(`Product:` + ` ` + `${this.a * this.b}`);
+  };
+  this.division = function () {
+    console.log(`Quotient:` + ` ` + `${this.a / this.b}`);
+  };
+}
+
+const numbers = new Calculator(2, 2);
+numbers.sum();
+numbers.subtraction();
+numbers.multiplication();
+numbers.division();
