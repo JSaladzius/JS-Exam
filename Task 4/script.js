25c64004679ad3b2fc72fb419968f58895110638
bulacio <postiklu@gmail.com>
2022-10-08 01:08:31 +0300

    Task 4; added fetch and  data render functions;

--- Task 4/script.js
+++ Task 4/script.js
@@ -9,4 +9,39 @@ Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), tur
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
+const ENDPOINT = "cars.json";
+
+function getCars() {
+  fetch(ENDPOINT)
+    .then((response) => response.json())
+    .then((cars) => renderCars(cars));
+}
+getCars();
+
+function renderCars(cars) {
+  const output = document.getElementById("output");
+  output.innerHTML = "";
+  console.log(cars);
+
+  cars.forEach((maker) => {
+    const brandBox = document.createElement("div");
+    brandBox.className = "brandBox";
+    const brandName = document.createElement("h2");
+    brandName.className = "brandName";
+    brandName.innerText = maker.brand;
+    const carList = document.createElement("ol");
+    carList.className = "carList";
+    console.log(maker.models);
+    const models = maker.models;
+
+    models.forEach((model) => {
+      const modelList = document.createElement("li");
+      modelList.className = "modelsList";
+      modelList.innerText = model;
+      carList.append(modelList);
+    });
+
+    brandBox.append(brandName, carList);
+    output.append(brandBox);
+  });
+}
